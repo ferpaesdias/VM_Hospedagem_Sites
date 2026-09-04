fc8fd5369d07a66a8e0e64687c1ab0dc35cbe6ff
Fernando Dias <ferpdias@gmail.com>
2026-09-04 19:23:50 -0300

    Ajuste no texto

--- Cookbook/gerenciar_usuarios_ftp.sh
+++ Cookbook/gerenciar_usuarios_ftp.sh
@@ -1,24 +1,59 @@
+#!/bin/bash
+
+###############################################################################
+### 
+###      Script para a criação de turma e usuários para o FTP
+###  
+###  
+### 
+###  O usuário permite que o aluno faça o upload do seu projeto (site) via 
+###  FTP.
+### 
+###  O diretório Home do usuário será o diretório /projetos/turma/nome_do_aluno
+### 
+###  O diretório Home será onde o aluno fará o upload dos arquivos e onde 
+###  estará hospedado o seu projeto.
+### 
+###  Formato do usuário: O usuário dos alunos (FTP) será o mesmo da primeira 
+###  parte do e-mail 
+###  educacional (sem o domínio), porém, sem o ponto separando o nome e 
+###  sobrenome.
+###  Exemplo: O usuário do aluno Fulano da Silva, que possui o email 
+###  fulano.dsilva@senac.sp.br, será fulanodsilva.
+### 
+###
+###  Modo de uso:
+###    Para inserir somente um aluno:
+###    $ ./gerenciar_usuarios_ftp --add turma nome_do_aluno
+###   
+###    Para remover um somente um aluno:
+###    $ ./gerenciar_usuarios_ftp --rm turma nome_do_aluno
+###   
+###    Para inserir somente um grupo de alunos usando um arquivo .csv:
+###    $ ./gerenciar_usuarios_ftp --add --csv arquivo.csv
+###   
+###    Para remover um somente um alunos usando um arquivo .csv:
+###    $ ./gerenciar_usuarios_ftp --csv arquivo.csv
+###
+###  
+###  Formato do arquivo .csv:
+###    turma01,nome_do_aluno01
+###    turma02,nome_do_aluno02
+###    turma02,nome_do_aluno03
+###    turma02,nome_do_aluno04
+### 
+###############################################################################
+
+$1=turma
+$2=nomeAluno
+
+echo 
 
 
 
+## Criação de usuários
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ## 3. Criação de usuários
 
 
 # Criar o diretório `/projetos/turma01`. Neste diretório ficará as pastas HOME dos usuários.
