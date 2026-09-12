317a2a9a05d0e2dff8b7f0e98b3ade603baf2ee4
Fernando Dias <ferpdias@gmail.com>
2026-09-12 19:33:08 -0300

    Criação das funções: log_erro, log_info, exibir_ajuda e checar_root

--- Cookbook/gerenciar_usuarios_ftp.sh
+++ Cookbook/gerenciar_usuarios_ftp.sh
@@ -54,12 +54,80 @@
 ###
 ###############################################################################
 
-# Função que verifica se o usuário é root:
-function verifica_usuario_root() {
-  if [[ $EUID -ne 0 ]]; then
-    echo "Este script deve ser executado como root." >&2
-    exit 1
-  fi  
+# Variáveis de entrada
+#USUARIO_TURMA=$1
+#USUARIO_NOME=$2
+ARQUIVO_LOG="vm_hospedagem.log"
+
+# -------------------------------------------
+# Função que imprime mensagem informativa formatada.  Os logs vão para o arquivo vm_hospedagem.log
+function log_erro() {
+  local mensagem_erro=$*
+  local data_hora
+  data_hora=$(date '+[%Y-%m-%d %H:%M:%S]')
+  printf "%s [ERRO] %s\n" "${data_hora}" "${mensagem_erro}" >> ${ARQUIVO_LOG}
+}
+# log_erro "Mensagem de erro"   TODO: remover comentários
+
+# -------------------------------------------
+
+# Função que imprime mensagem informativa formatada. Os logs vão para o arquivo vm_hospedagem.log
+function log_info() {
+  local mensagem_info=$*
+  local data_hora
+  data_hora=$(date '+[%Y-%m-%d %H:%M:%S]')
+  printf "%s [INFO] %s\n" "${data_hora}" "${mensagem_info}" >> ${ARQUIVO_LOG}
+  # TODO: Ajustar a permissão do arquivo de log para quando o usuário não for root 
+}
+# log_info "Mensagem de erro"   TODO: remover comentários
+
+# -------------------------------------------
+
+# Função que exibe ajuda caso o usuário digite --help ou -h
+function exibir_ajuda() { 
+  
+  echo "Uso: $0 [--add | --rm] [--csv arquivo.csv] turma nome_aluno"
+  echo "       $0 --help"
+  echo ""
+  echo "Opções:"
+  echo "  --add           Adiciona um aluno ao servidor FTP"
+  echo "  --rm            Remove um aluno do servidor FTP"
+  echo "  --csv arquivo   Lê os alunos de um arquivo CSV (turma,nome_aluno)"
+  echo "  --help, -h      Exibe esta mensagem de ajuda"
 }
 
-verifica_usuario_root
+# Verifica se o usuário digitou --help ou -h
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  exibir_ajuda
+  exit 0
+fi
+
+# -------------------------------------------
+
+# Função que verifica se o usuário é root
+function checar_root() {
+    if [[ $EUID -ne 0 ]]; then
+    log_erro "Este script deve ser executado como root."
+    echo "Erro: Este script deve ser executado como root."
+    exit 1
+  fi
+}
+checar_root
+# -------------------------------------------
+
+function validar_nome_turma() { :; }
+function validar_nome_aluno() { :; }
+function validar_arquivo_csv() { :; }
+function usuario_existe() { :; }
+function diretorio_existe() { :; }
+function garantir_shell_ftp() { :; }
+function garantir_diretorio_turma() { :; }
+function criar_aluno() { :; }
+function confirmar_operacao() { :; }
+function remover_aluno() { :; }
+function parsear_argumentos() { :; }
+function main() { :; }
+function processar_csv_add() { :; }
+function processar_csv_rm() { :; }
+
+
