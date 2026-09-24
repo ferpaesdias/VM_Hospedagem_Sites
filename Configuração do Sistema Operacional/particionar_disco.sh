5425e788244fafbf6ac82ead6496a77a2877229e
Fernando Dias <ferpaesdias@gmail.com>
2026-09-24 11:22:52 -0300

    Cabeçalho do script

---
+++ Configuração do Sistema Operacional/particionar_disco.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###############################################################################
+###
+###      Script para particionar o disco onde serão inserido os projetos
+###
+###  --------------------------------------------------------------------------
+###  INSTRUÇÕES
+###  --------------------------------------------------------------------------
+###
+###  Insira um disco no computador, se for máquina virtual, crie um disco 
+###  virtual e o anexe à VM.
+###
+###  Execute o script usando um dos comandos abaixo:
+###     Como root: bash particionar_disco.sh
+###     Usando o sudo: sudo bash particionar_disco.sh
+###
+###############################################################################
+
+
